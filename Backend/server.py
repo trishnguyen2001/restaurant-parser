92cf8b4cd5d1f7da45b8f98c092f70495d302fb0
Trish Nguyen <trishnguyen18@gmail.com>
2023-11-24 13:38:48 -0800

    Code cleanup

--- Backend/server.py
+++ Backend/server.py
@@ -13,19 +13,19 @@ from MyExprVisitor import MyExprVisitor
 
 app = Flask(__name__)
 CORS(app)
+Session(app)
 app.config["SESSION_PERMANENT"] = False
 app.config["SECRET_KEY"] = "password"
-# app.config["SESSION_COOKIE_NAME"] = "sessioncookie"
 app.config["SESSION_TYPE"] = "filesystem"
 app.session_cookie_name = "sessioncookie"
 app.session_interface = RestaurantSessionInterface()
-Session(app)
 global sessionInterface
         
 
 @app.route('/', methods=['GET', 'POST'])
 def user_input():
 
+    # executes MyExprVisitor for parsing
     def handleUserInput(r, prompt):
         print("prompt: " + prompt)
         if (prompt == "testing"):
@@ -34,33 +34,39 @@ def user_input():
             lexer = ExprLexer(InputStream(prompt))
             stream = CommonTokenStream(lexer)
             parser = ExprParser(stream)
-            print("parser.prog() starting...")
+            print("Parsing starting...")
             tree = parser.prog()
-            print("MyExprVisitor(r).visitProg(tree) starting...")
-            print("Session retrieved\nbalance = ", r.balance, 
-                    "\n ingrList = ", r.ingredient_inventory, 
-                    "\n dishList = ", r.dish_inventory)
-            parserRes = MyExprVisitor(r).visitProg(tree)
-            session['parsedResponse'] = parserRes
+            session['parsedResponse'] = MyExprVisitor(r).visitProg(tree)
             print("Parsing finished!")
             time.sleep(3)
 
     data = request.get_json()
     try:
-        # parse user prompt
         print("Flask server request received")
+
+        # opens session
         sessionInterface = RestaurantSessionInterface(Restaurant())
         rSession = sessionInterface.open_session(app, request)
+        print("Active Session: \nbalance = ", rSession.restaurant.balance, 
+                    "\n ingrList = ", rSession.restaurant.ingredient_inventory, 
+                    "\n dishList = ", rSession.restaurant.dish_inventory)
         rSession['parsedResponse'] = None
-        # print('restaurant = $', rSession.restaurant.balance)
+
+        # run parser based on user's input
         handleUserInput(rSession.restaurant, data['userPrompt'])
+        # validates response from parser
         if (session.get("parsedResponse") == None):
             session['parsedResponse'] = "No response"
         print("parsedResponse = ", session.get("parsedResponse"))
+
+        # saves session with current restaurant data
         sessionInterface.save_session(app, rSession, make_response())
+
     except Exception as e:
         session['parsedResponse'] = "ERROR: " + str(e) 
         traceback.print_exc()
+
+    # sends response back to React server
     return(jsonify(session.get("parsedResponse")))
 
 
@@ -69,4 +75,3 @@ def user_input():
 if __name__ == '__main__':
     app.debug = True
     app.run()
-    # session['balance'] = 1000

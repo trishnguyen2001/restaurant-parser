651a483ff4ff3741dced1835cfaa4277abef9215
Trish Nguyen <trishnguyen18@gmail.com>
2023-11-27 17:41:58 -0800

    only one (DIGIT)+ definition in Expr.g4

--- Backend/MyExprVisitor.py
+++ Backend/MyExprVisitor.py
@@ -40,11 +40,11 @@ class MyExprVisitor(ExprVisitor):
         elif ctx.OP_NEWDISH():
             print("[MyExprVisitor] visitCommand(): ctx.OP_NEWDISH")
             #print("dish name = ", ctx.dishname.text)
-            #print("price = " , ctx.price.text)
+            # print("price = " , ctx.price.text)
             #print("cooking method = " , ctx.cooking_method.text)
-            #print("temp = " , ctx.temperature.text)
+            # print("temp = " , ctx.temperature.text)
             #print("cooking time = " , ctx.cooking_duration.text)
-            response = self.restaurant.new_dish(int(ctx.price.text), int(ctx.temperature.text))
+            response = self.restaurant.cook(int(ctx.price.text), int(ctx.temperature.text))
 
         self.stack.append(response)
         print("[MyExprVisitor] visitCommand() --> FINISHED")
